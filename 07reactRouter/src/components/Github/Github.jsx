edf3205cfa03fe32273071631feff5b2932fcdbd
Lukesh <lukeshankamwar2103@gmail.com>
2024-09-13 14:08:43 +0530

    React router

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    // const [data ,setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/luke2103')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data)
+    //             setData(data)})
+    // },[])
+
+       const data =  useLoaderData()
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+            Github followers : {data.followers}
+            <img className='text-center' src={data.avatar_url}
+            alt="git picture" />
+        </div>
+         
+
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+        
+    const response = await fetch('https://api.github.com/users/luke2103')
+    return response.json()
+}
